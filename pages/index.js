8ff94a82c81f2381110e34419a87dde099468ef3
Kaushal Bhatt <meetkaushal@gmail.com>
2020-08-15 17:55:12 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,19 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
+    
+    <div>
+      Who Let the Dogs Out
+
+
+
+
+    </div>
+    
+    
+    
+    
+    
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
